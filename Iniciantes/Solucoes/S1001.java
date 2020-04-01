4025aaf89202bab44553a46a55c484cf1b8fc705
Anderson <62722855+anderdot@users.noreply.github.com>
2020-04-01 04:06:32 -0300

    1001

---
+++ Iniciantes/Solucoes/S1001.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class S1001 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt(),
+            b = scanner.nextInt();
+        scanner.close();
+        
+        System.out.println("X = " + (a + b));
+    }
+}
