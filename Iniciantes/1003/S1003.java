060d380ab655c2dbc55277603c967b9e21afbcfa
Anderson <62722855+anderdot@users.noreply.github.com>
2020-04-01 05:53:00 -0300

    1003

---
+++ Iniciantes/1003/S1003.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class S1003
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt(), B = scanner.nextInt();
+        scanner.close();
+
+        int SOMA = A + B;
+        
+        System.out.println("SOMA = " + SOMA);
+    }
+}
