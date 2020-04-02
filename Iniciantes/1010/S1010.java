34961d8ad28d8b220fdf8034fda5a06c9316290e
Anderson <62722855+anderdot@users.noreply.github.com>
2020-04-02 01:52:15 -0300

    1010

---
+++ Iniciantes/1010/S1010.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class S1010 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.nextInt(); //cod não usado
+        int valor1 = scanner.nextInt();
+        double preco1 = scanner.nextDouble();
+
+        scanner.nextInt(); //cod não usado
+        int valor2 = scanner.nextInt();
+        double preco2 = scanner.nextDouble(),
+
+        pagar = (valor1 * preco1) + (valor2 * preco2);
+        scanner.close();
+
+        System.out.format("VALOR A PAGAR: R$ %.2f%n", pagar);
+    }
+}
