dbbad9a5cebd8c0be221d1d875fbc99621dde7ef
Anderson <62722855+anderdot@users.noreply.github.com>
2020-04-03 04:42:29 -0300

    1014

---
+++ Iniciantes/1014/S1014.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class S1014 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        int x = scanner.nextInt();
+        double y = scanner.nextDouble();
+        scanner.close();
+        
+        System.out.format("%.3f km/l%n", (x/y));
+    }
+}
