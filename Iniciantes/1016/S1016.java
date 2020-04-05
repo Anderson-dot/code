ec53b02be203a4e0bc936126b0c8c24b44825856
Anderson <62722855+anderdot@users.noreply.github.com>
2020-04-05 16:54:15 -0300

    1016

---
+++ Iniciantes/1016/S1016.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class S1016 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+
+        x *= 2;
+
+        System.out.println(x + " minutos");
+    }
+}
