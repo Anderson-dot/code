1ec8de55027d9614a5623dee93df98ba450bebc2
Anderson <62722855+anderdot@users.noreply.github.com>
2020-04-06 00:10:22 -0300

    1017

---
+++ Iniciantes/1017/S1017.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class S1017 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int tempo = scanner.nextInt(),
+            velocidade = scanner.nextInt();
+        scanner.close();
+
+        System.out.format("%.3f", ((tempo * velocidade) / 12.0));
+    }
+}
