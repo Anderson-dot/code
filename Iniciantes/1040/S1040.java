2c0320b2c557f7b36b5d8cd7f642d96028bf3a06
Anderson <62722855+anderdot@users.noreply.github.com>
2020-04-10 20:34:15 -0300

    Média 3
    1040

--- Iniciantes/1040/S1040.java
+++ Iniciantes/1040/S1040.java
@@ -1,49 +1,57 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class S1040 
 {
     public static void main(String[] args) 
     {
         Scanner scanner = new Scanner(System.in);
-        scanner.useLocale(Locale.ENGLISH);
-		Locale.setDefault(new Locale("en", "US"));
 
         double 
-            n1 = (scanner.nextDouble() * 2.0),
-            n2 = (scanner.nextDouble() * 3.0),
-            n3 = (scanner.nextDouble() * 4.0),
-            n4 = scanner.nextDouble();
+            n1 = (scanner.nextDouble() * 2) / 10,
+            n2 = (scanner.nextDouble() * 3) / 10,
+            n3 = (scanner.nextDouble() * 4) / 10,
+            n4 = (scanner.nextDouble() * 1) / 10,
+            media = n1 + n2 + n3 + n4;
 
-        double media = (n1 + n2 + n3 + n4) / 10.0;
-        System.out.format("Media: %.1f%n", Math.floor(media));  
-
-        if (media >= 7) 
+        if (media * 10 - Math.floor(media * 10) > 0.5001) //da 5% aekkKKkk
         {
-            System.out.println("Aluno aprovado.");   
+            media = Math.ceil(media * 10) / 10;
         }
-        else if (media < 5) 
+        else 
+        {
+            media = Math.floor(media * 10) / 10;
+        }
+
+        System.out.printf("Media: %.1f%n", media);
+
+        if (media < 5.0) 
         {
             System.out.println("Aluno reprovado.");
         }
-        else if (media >= 5 && media <= 6.9)
+
+        if (media >= 7.0) 
         {
-            System.out.println("Aluno em exame.");
+            System.out.printf("Aluno aprovado.%n");
+        }
+        else if (media >= 5.0) 
+        {
+            System.out.printf("Aluno em exame.%n");
             double nExame = scanner.nextDouble();
             scanner.close();
-            System.out.format("Nota do exame: %.1f%n", Math.floor(nExame));
 
-            media = (media + nExame) / 2.0;
+            System.out.printf("Nota do exame: %.1f%n", nExame);
+            media = (media + nExame) / 2;
 
             if (media >= 5) 
             {
                 System.out.println("Aluno aprovado.");
             }
-            else
+            else 
             {
-                System.out.println("Aluno aprovado.");
+                System.out.println("Aluno reprovado.");
             }
 
-            System.out.format("Media final: %.1f%n", media);
+            System.out.printf("Media final: %.1f%n",media);
         }
     }
 }
