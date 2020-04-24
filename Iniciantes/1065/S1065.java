188ac58e15c92209e1f85d8010069aa3f6f471c0
Anderson <62722855+anderdot@users.noreply.github.com>
2020-04-23 23:59:59 -0300

    Pares entre Cinco Números
    1065

--- Iniciantes/1065/S1065.java
+++ Iniciantes/1065/S1065.java
@@ -4,6 +4,19 @@ public class S1065
 {
     public static void main(String[] args) 
     {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int pares = 0;
+
+        for (int i = 0; i < 5; i++) 
+        {
+            if (scanner.nextInt() % 2 == 0) 
+            {
+                pares++;
+            }
+        }
+
+        scanner.close();
+        System.out.println(pares + " valores pares");
     }
 }
