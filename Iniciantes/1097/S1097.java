10c85ad9655d6b97f7651e4941451e66e6a8c108
Anderson <62722855+anderdot@users.noreply.github.com>
2020-04-26 05:03:17 -0300

    Sequencia IJ 3
    1097

---
+++ Iniciantes/1097/S1097.java
@@ -0,0 +1,14 @@
+
+public class S1097 
+{
+    public static void main(String[] args) 
+    {
+        for (int i = 1; i <= 9; i += 2) 
+        {
+            for (int j = i + 6; j > (i + 3); j--) 
+            {
+                System.out.println("I=" + i + " J=" + j);
+            }
+        }
+    }
+}
