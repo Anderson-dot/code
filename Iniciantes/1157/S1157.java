c301ef056fba491acfaaaddbda865ae264403cbf
Anderson <62722855+anderdot@users.noreply.github.com>
2020-05-08 04:54:11 -0300

    Divisores I
    1157

---
+++ Iniciantes/1157/S1157.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class S1157
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        scanner.close();
+
+        for (int i = 1; i <= N; i++)
+        {
+            if (N%i == 0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
